0f4b06282fb8921f1b5be32c05174c025dc1b42d
ATajadod94 <Atajadod94@gmail.com>
2018-05-14 16:08:56 -0400

    Updated Trial

--- src/trial.m
+++ src/trial.m
@@ -14,6 +14,12 @@ classdef trial < data
         
         %Saccade features
         num_saccades
+        num_fixation
+        sample_time
+        issaccade
+        isfixation
+        issaccadeorfixation
+        num_samples
         saccade_location
         saccade_duration_variation
     end
@@ -28,12 +34,18 @@ classdef trial < data
         function plot(obj)
             [obj.theta, obj.rho] = cart2pol(obj.x, obj.y);
             num_samples = length(obj.theta);
-            num_colors = ceil(num_samples / 100);
+            color_per_sample = 10;
+            num_colors = ceil(num_samples / color_per_sample);
             colorvector = [255:-255/num_colors:0;zeros(1,num_colors+1) ;0:255/num_colors:255]'/255;       
             for i =2:num_colors-1
-              polarplot(obj.theta((i-1)*100:i*100) , obj.rho((i-1)*100:i*100), 'color', colorvector(i,:));
+              polarplot(obj.theta((i-1)*color_per_sample:i*color_per_sample) ...
+              , obj.rho((i-1)*color_per_sample:i*color_per_sample), 'color', ...
+              colorvector(i,:));
               hold on;
             end
+        end
+        function get_polar(obj)
+          [obj.theta, obj.rho] = cart2pol(obj.x, obj.y);
         end
         function number_of_fixation(obj)
             obj.num_fixations = length(obj.parent.data.fixation_start.(obj.trial_fieldname));
@@ -64,7 +76,37 @@ classdef trial < data
             obj.fixation_duration_variation = zscore(double(obj.fixation_duration));
                     
         end
-        
+        function get_issaccade(obj)
+            obj.saccade_start = obj.parent.data.saccade_start.(obj.trial_fieldname);
+            obj.sample_time = obj.parent.data.datfile(obj.trial_no).Events.sttime - obj.parent.data.datfile(obj.trial_no).Events.sttime(1);
+            obj.num_samples  = length(obj.x);
+            obj.issaccade = zeros(size(obj.sample_time));
+            for i = 1:obj.num_saccades
+                time_of_saccade = [obj.saccade_start(i), obj.saccade_start(i) + obj.saccade_duration(i)];
+                idx = obj.sample_time >= time_of_saccade(1) & obj.sample_time <= time_of_saccade(2);
+                if isempty(find(idx,1))
+                    idx = find(obj.sample_time >= time_of_saccade(1),1);
+                end
+                obj.issaccade(idx) = i ;
+            end
+        end
+        function get_isfixation(obj)
+            obj.fixation_start = obj.parent.data.fixation_start.(obj.trial_fieldname);
+            obj.num_samples  = length(obj.x);
+            obj.isfixation = zeros(size(obj.sample_time));
+            obj.num_fixation = length(obj.fixation_start);
+            for i = 1:obj.num_fixation
+                time_of_saccade = [obj.fixation_start(i), obj.fixation_start(i) + obj.fixation_duration(i)];
+                idx = obj.sample_time >= time_of_saccade(1) & obj.sample_time <= time_of_saccade(2);
+                if isempty(find(idx,1))
+                    idx = find(obj.sample_time >= time_of_saccade(1),1);
+                end
+                obj.isfixation(idx) = i ;
+            end
+            obj.issaccadeorfixation = zeros(size(obj.sample_time));
+            obj.issaccadeorfixation(find(obj.issaccade)) = 1;
+            obj.issaccadeorfixation(find(obj.isfixation)) = -1;
+       end
         function deviation_of_duration_of_saccade(obj)
             if isempty(obj.saccade_duration)
                 duration_of_saccade(obj)
